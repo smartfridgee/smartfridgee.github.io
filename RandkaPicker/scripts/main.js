fdde1a22950d2b949215fc2076e28994604a5106
smartfridgee <70762397+smartfridgee@users.noreply.github.com>
2021-05-03 02:04:45 +0200

    Update

--- RandkaPicker/scripts/main.js
+++ RandkaPicker/scripts/main.js
@@ -1,7 +1,9 @@
 //loadingScreen
 const loadingScreen = setTimeout(function(){
 	overlay = document.getElementById("loadingScreen");
+	shark = document.getElementById("loadingShark");
 	overlay.classList.toggle("off");
+	shark.classList.toggle("off");
 	setTimeout(function(){
 		overlay.remove();
 	}, 1000)
