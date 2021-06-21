656acfdca6540eaeac7a401fece53521eab9f6f0
smartfridgee <70762397+smartfridgee@users.noreply.github.com>
2021-06-21 02:28:19 +0200

    home 2.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,23 @@
-const helloSection = document.getElementById("hello");
-const starsSection = document.getElementById("stars");
+const navBar = document.querySelector('nav');
 
-document.addEventListener('scroll', () => {
-	let value = window.scrollY;
-	let helloValue = 300;
-	let headerValue = 159;
-	helloSection.style.top = helloValue + value * 0.60 + 'px';
-	starsSection.style.top = value * 0.70 + 'px';
+window.onscroll = function(e) {
+	// print "false" if direction is down and "true" if up
+	console.log(this.oldScroll > this.scrollY);
+	if(this.oldScroll > this.scrollY == false){
+		navBar.classList.add('hidden');
+	}
+	else{
+		navBar.classList.remove('hidden');
+	}
+	this.oldScroll = this.scrollY;
+}
+
+const sections = document.querySelectorAll('section');
+
+let sectionIDs = [];
+
+sections.forEach(item => {
+	sectionIDs.push(item.id);
 })
+
+const upButton = document.getElementById('up');
