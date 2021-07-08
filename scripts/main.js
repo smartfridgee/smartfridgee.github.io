cabe8a6f386f5d8bf99439c1d01217564fb933a9
smartfridgee <70762397+smartfridgee@users.noreply.github.com>
2021-07-08 23:03:53 +0200

    update 3.0

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,28 @@ const navBar = document.querySelector('nav');
 
 window.onscroll = function(e) {
 	// print "false" if direction is down and "true" if up
-	console.log(this.oldScroll > this.scrollY);
 	if(this.oldScroll > this.scrollY == false){
 		navBar.classList.add('hidden');
+		navTrigger.style.pointerEvents = "inherit";
 	}
 	else{
 		navBar.classList.remove('hidden');
+		navTrigger.style.pointerEvents = "none";
 	}
 	this.oldScroll = this.scrollY;
 }
+
+const navTrigger = document.querySelector('.trigger');
+
+navTrigger.addEventListener('mouseenter', ()=>{
+	navBar.classList.remove('hidden');
+	navTrigger.style.pointerEvents = "none";
+})
+
+navBar.addEventListener('mouseout', ()=>{
+	navBar.classList.add('hidden');
+	navTrigger.style.pointerEvents = "inherit";
+})
 
 const sections = document.querySelectorAll('section');
 
@@ -20,4 +33,28 @@ sections.forEach(item => {
 	sectionIDs.push(item.id);
 })
 
-const upButton = document.getElementById('up');
+let sectionCounter = 0;
+
+function changeSection(direction){
+	if(direction == 0){
+		window.scrollTo(0,0);
+		sectionCounter = 0;
+		return;
+	}
+	
+	if(direction == 1 && sectionCounter < sections.length-1){
+		let target = document.getElementById(sections[sectionCounter + direction].id);
+		target.scrollIntoView({behavior: "smooth", block: "start"});
+		sectionCounter += direction;
+		return;
+	}
+	
+	if(direction == -1 && sectionCounter > 0){
+		let target = document.getElementById(sections[sectionCounter + direction].id);
+		target.scrollIntoView({behavior: "smooth", block: "start"});
+		sectionCounter += direction;
+		return;
+	}
+	
+	
+}
