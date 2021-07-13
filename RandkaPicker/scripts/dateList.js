a715634a9acac989ac154d52bfdecdbe99b91d33
smartfridgee <70762397+smartfridgee@users.noreply.github.com>
2021-07-13 02:52:46 +0200

    Delete RandkaPicker directory

--- RandkaPicker/scripts/dateList.js
+++
@@ -1,6 +0,0 @@
-let dateList = [
-	{ name: 'Randeczka nad jeziorkiem', alone: 'nie', anxiety: 'nie', weather: 'tak', strong: 'tak', food: 'nie'},
-	{ name: 'Randeczka w domciu', alone: 'nie', anxiety: 'nie', weather: 'nie', strong: 'tak', food: 'nie'},
-	{ name: 'Randeczka w parku', alone: 'nie', anxiety: 'nie', weather: 'tak', strong: 'tak', food: 'nie'},
-	{ name: 'Randeczka u mnie', alone: 'nie', anxiety: 'nie', weather: 'nie', strong: 'tak', food: 'nie'}
-]
