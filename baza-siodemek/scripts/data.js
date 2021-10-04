e272b7ed0e22c251fc926366f5d2259192a9824f
smartfridgee <70762397+smartfridgee@users.noreply.github.com>
2021-10-05 00:55:16 +0200

    version 1.0

---
+++ baza-siodemek/scripts/data.js
@@ -0,0 +1,167 @@
+// const data_pkpic = [
+//     {series: 'EP07', number: '345', type: '303E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '355', type: '303E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '357', type: '303E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '382', type: '303E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '387', type: '303E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '435', type: '303E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '456', type: '303E', lamps: 'TV', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '505', type: '303E', lamps: 'Olkol', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '506', type: '303E', lamps: 'Olkol', paintjob: 'Standardowe IC', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '513', type: '303E', lamps: 'Olkol', paintjob: 'Standardowe IC', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '517', type: '303E', lamps: 'Olkol', paintjob: 'Standardowe IC', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '1012', type: '4E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '1014', type: '303E', lamps: 'Olkol', paintjob: 'Standardowe IC', panto: 'Połówkowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '1020', type: '4E', lamps: 'TV', paintjob: 'Standardowe IC', panto: 'Połówkowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '1024', type: '303E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '1049', type: '4E', lamps: 'Olkol', paintjob: 'Niebieskie Czoło', panto: 'Połówkowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '1058', type: '4E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EP07', number: '1067', type: '4E', lamps: 'Olkol', paintjob: 'Standardowe IC', panto: 'Połówkowe', railway: 'PKP Intercity'},
+//     {series: 'EU07', number: '030', type: '4E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EU07', number: '072', type: '4E', lamps: 'Wiadra', paintjob: 'Standardowe IC', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EU07', number: '334', type: '303E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+//     {series: 'EU07A', number: '001', type: '303E', lamps: 'Olkol', paintjob: 'Standardowe IC', panto: 'Połówkowe', railway: 'PKP Intercity'},
+//     {series: 'EU07A', number: '002', type: '303E', lamps: 'Olkol', paintjob: 'Standardowe IC', panto: 'Połówkowe', railway: 'PKP Intercity'},
+//     {series: 'EU07A', number: '003', type: '303E', lamps: 'Olkol', paintjob: 'Standardowe IC', panto: 'Połówkowe', railway: 'PKP Intercity'}
+// ];
+
+// const data_kolprem = [
+//     {series: 'EU07', number: '012', type: '4E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '062', type: '4E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '078', type: '4E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '134', type: '4E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '139', type: '4E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '144', type: '4E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '158', type: '4E', lamps: 'Wiadra', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '183', type: '4E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '185', type: '4E', lamps: 'ZEZ', paintjob: 'Czekoladowe ex.PR', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '186', type: '4E', lamps: 'ZEZ', paintjob: 'Czekoladowe ex.PR', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '399', type: '303E', lamps: 'TV', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '437', type: '303E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '411', type: '303E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '460', type: '303E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '469', type: '303E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '493', type: '303E', lamps: 'ZEZ', paintjob: 'DLA', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '499', type: '303E', lamps: 'TV', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+//     {series: 'EU07', number: '528', type: '303E', lamps: 'ZEZ', paintjob: 'Orion Kolej (?)', panto: 'Nożycowe', railway: 'PUK Kolprem'}
+// ];
+
+// const data_pcc = [
+//     {series: 'EU07', number: '155', type: '4E', lamps: 'ZEZ', paintjob: 'Olkol Lok. Zastępcza', panto: 'Nożycowe', railway: 'PCC Intermodal'},
+//     {series: 'EU07', number: '467', type: '303E', lamps: 'ZEZ', paintjob: 'PCC Intermodal "Zbyszek"', panto: 'Nożycowe', railway: 'PCC Intermodal'},
+//     {series: 'EU07', number: '484', type: '303E', lamps: 'ZEZ', paintjob: 'PCC Intermodal', panto: 'Nożycowe', railway: 'PCC Intermodal'},
+//     {series: 'EU07', number: '512', type: '303E', lamps: 'ZEZ', paintjob: 'PCC Intermodal', panto: 'Nożycowe', railway: 'PCC Intermodal'}
+// ]
+
+// const data_hsl = [
+//     {series: 'EU07', number: '403', type: '303E', lamps: 'ZEZ', paintjob: 'Brązowe HSL', panto: 'Nożycowe', railway: 'HSL Polska'},
+//     {series: 'EU07', number: '522', type: '303E', lamps: 'ZEZ', paintjob: 'Brązowe HSL', panto: 'Nożycowe', railway: 'HSL Polska'}
+// ];
+
+// const data_pkpc = [
+//     {series: 'EU07', number: '128', type: '4E', lamps: 'TV', paintjob: 'Zielone', panto: 'Nożycowe', railway: 'PKP Cargo'},
+//     {series: 'EU07', number: '195', type: '4E', lamps: 'ZEZ', paintjob: 'Turkusowa', panto: 'Nożycowe', railway: 'PKP Cargo'},
+//     {series: 'EU07', number: '241', type: '4E', lamps: 'Wiadra', paintjob: 'Żółte Czoło', panto: 'Nożycowe', railway: 'PKP Cargo'},
+//     {series: 'EU07', number: '308', type: '303E', lamps: 'ZEZ', paintjob: 'Zielone', panto: 'Nożycowe', railway: 'PKP Cargo'},
+//     {series: 'EU07', number: '1512', type: '303E', lamps: 'ZEZ', paintjob: 'Nowy Schemat PKPC', panto: 'Nożycowe', railway: 'PKP Cargo'}
+// ];
+
+// const data_ecco = [
+//     {series: 'EU07', number: '047', type: '4E', lamps: 'ZEZ', paintjob: 'Niebieskie ECCO Rail', panto: 'Nożycowe', railway: 'ECCO Rail'},
+//     {series: 'EU07', number: '088', type: '4E', lamps: 'ZEZ', paintjob: 'Niebieskie ECCO Rail', panto: 'Nożycowe', railway: 'ECCO Rail'},
+//     {series: 'EU07', number: '080', type: '4E', lamps: 'ZEZ', paintjob: 'Biała Dama', panto: 'Nożycowe', railway: 'ECCO Rail'},
+//     {series: 'EU07', number: '141', type: '4E', lamps: 'ZEZ', paintjob: 'Biała Dama', panto: 'Nożycowe', railway: 'ECCO Rail'},
+//     {series: 'EU07', number: '145', type: '4E', lamps: 'ZEZ', paintjob: 'Biała Dama', panto: 'Nożycowe', railway: 'ECCO Rail'},
+//     {series: 'EU07', number: '166', type: '4E', lamps: 'ZEZ', paintjob: 'Biała Dama', panto: 'Nożycowe', railway: 'ECCO Rail'},
+//     {series: 'EU07', number: '167', type: '4E', lamps: 'ZEZ', paintjob: 'Biała Dama', panto: 'Nożycowe', railway: 'ECCO Rail'},
+//     {series: 'EU07', number: '465', type: '303E', lamps: 'ZEZ', paintjob: 'Zielone Trapez', panto: 'Nożycowe', railway: 'ECCO Rail'}
+// ];
+
+const data = [
+    //PKP INTERCITY
+    {series: 'EP07', number: '174', type: '4E', lamps: 'ZEZ', paintjob: 'Zielone (Szpinak)', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '330', type: '303E', lamps: 'ZEZ', paintjob: 'Zielone Trapez', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '338', type: '303E', lamps: 'ZEZ', paintjob: 'Zielone Trapez', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '345', type: '303E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '355', type: '303E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '356', type: '303E', lamps: 'ZEZ', paintjob: 'Żółte Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '357', type: '303E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '376', type: '303E', lamps: 'ZEZ', paintjob: 'Zielone', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '382', type: '303E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '387', type: '303E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '435', type: '303E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '442', type: '303E', lamps: 'ZEZ', paintjob: 'Żółte Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '456', type: '303E', lamps: 'TV', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '505', type: '303E', lamps: 'Olkol', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '506', type: '303E', lamps: 'Olkol', paintjob: 'Standardowe IC', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '513', type: '303E', lamps: 'Olkol', paintjob: 'Standardowe IC', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '517', type: '303E', lamps: 'Olkol', paintjob: 'Standardowe IC', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '1012', type: '4E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '1014', type: '303E', lamps: 'Olkol', paintjob: 'Standardowe IC', panto: 'Połówkowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '1020', type: '4E', lamps: 'TV', paintjob: 'Standardowe IC', panto: 'Połówkowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '1024', type: '303E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '1049', type: '4E', lamps: 'Olkol', paintjob: 'Niebieskie Czoło', panto: 'Połówkowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '1058', type: '4E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EP07', number: '1067', type: '4E', lamps: 'Olkol', paintjob: 'Standardowe IC', panto: 'Połówkowe', railway: 'PKP Intercity'},
+    {series: 'EU07', number: '030', type: '4E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EU07', number: '072', type: '4E', lamps: 'Wiadra', paintjob: 'Standardowe IC', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EU07', number: '334', type: '303E', lamps: 'ZEZ', paintjob: 'Niebieskie Czoło', panto: 'Nożycowe', railway: 'PKP Intercity'},
+    {series: 'EU07A', number: '001', type: '303E', lamps: 'Olkol', paintjob: 'Standardowe IC', panto: 'Połówkowe', railway: 'PKP Intercity'},
+    {series: 'EU07A', number: '002', type: '303E', lamps: 'Olkol', paintjob: 'Standardowe IC', panto: 'Połówkowe', railway: 'PKP Intercity'},
+    {series: 'EU07A', number: '003', type: '303E', lamps: 'Olkol', paintjob: 'Standardowe IC', panto: 'Połówkowe', railway: 'PKP Intercity'},
+
+    //PKP CARGO
+    {series: 'EU07', number: '128', type: '4E', lamps: 'TV', paintjob: 'Zielone', panto: 'Nożycowe', railway: 'PKP Cargo'},
+    {series: 'EU07', number: '195', type: '4E', lamps: 'ZEZ', paintjob: 'Turkusowe', panto: 'Nożycowe', railway: 'PKP Cargo'},
+    {series: 'EU07', number: '241', type: '4E', lamps: 'Wiadra', paintjob: 'Żółte Czoło', panto: 'Nożycowe', railway: 'PKP Cargo'},
+    {series: 'EU07', number: '308', type: '303E', lamps: 'ZEZ', paintjob: 'Zielone', panto: 'Nożycowe', railway: 'PKP Cargo'},
+    {series: 'EU07', number: '1504', type: '303E', lamps: 'ZEZ', paintjob: 'Nowy Schemat PKPC', panto: 'Nożycowe', railway: 'PKP Cargo'},
+    {series: 'EU07', number: '1505', type: '303E', lamps: 'ZEZ', paintjob: 'Nowy Schemat PKPC', panto: 'Nożycowe', railway: 'PKP Cargo'},
+    {series: 'EU07', number: '1508', type: '303E', lamps: 'ZEZ', paintjob: 'Nowy Schemat PKPC', panto: 'Nożycowe', railway: 'PKP Cargo'},
+    {series: 'EU07', number: '1512', type: '303E', lamps: 'ZEZ', paintjob: 'Nowy Schemat PKPC', panto: 'Nożycowe', railway: 'PKP Cargo'},
+    {series: 'EU07', number: '1513', type: '303E', lamps: 'ZEZ', paintjob: 'Nowy Schemat PKPC', panto: 'Nożycowe', railway: 'PKP Cargo'},
+    {series: 'EU07', number: '1515', type: '303E', lamps: 'TV', paintjob: 'Nowy Schemat PKPC', panto: 'Nożycowe', railway: 'PKP Cargo'},
+    {series: 'EU07', number: '1517', type: '303E', lamps: 'ZEZ', paintjob: 'Nowy Schemat PKPC', panto: 'Nożycowe', railway: 'PKP Cargo'},
+    {series: 'EU07', number: '1518', type: '303E', lamps: 'ZEZ', paintjob: 'Nowy Schemat PKPC', panto: 'Nożycowe', railway: 'PKP Cargo'},
+    {series: 'EU07', number: '1527', type: '303E', lamps: 'ZEZ', paintjob: 'Nowy Schemat PKPC', panto: 'Nożycowe', railway: 'PKP Cargo'},
+
+    //PUK KOLPREM
+    {series: 'EU07', number: '012', type: '4E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '062', type: '4E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '078', type: '4E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '134', type: '4E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '139', type: '4E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '144', type: '4E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '158', type: '4E', lamps: 'Wiadra', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '183', type: '4E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '185', type: '4E', lamps: 'ZEZ', paintjob: 'Czekoladowe ex.PR', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '186', type: '4E', lamps: 'ZEZ', paintjob: 'Czekoladowe ex.PR', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '399', type: '303E', lamps: 'TV', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '437', type: '303E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '411', type: '303E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '460', type: '303E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '469', type: '303E', lamps: 'ZEZ', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '493', type: '303E', lamps: 'ZEZ', paintjob: 'DLA', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '499', type: '303E', lamps: 'TV', paintjob: 'Kolorowe Kolprem', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+    {series: 'EU07', number: '528', type: '303E', lamps: 'ZEZ', paintjob: 'Orion Kolej (?)', panto: 'Nożycowe', railway: 'PUK Kolprem'},
+
+    //PCC INTERMODAL
+    {series: 'EU07', number: '155', type: '4E', lamps: 'ZEZ', paintjob: 'Olkol Lok. Zastępcza', panto: 'Nożycowe', railway: 'PCC Intermodal'},
+    {series: 'EU07', number: '467', type: '303E', lamps: 'ZEZ', paintjob: 'PCC Intermodal "Zbyszek"', panto: 'Nożycowe', railway: 'PCC Intermodal'},
+    {series: 'EU07', number: '484', type: '303E', lamps: 'ZEZ', paintjob: 'PCC Intermodal', panto: 'Nożycowe', railway: 'PCC Intermodal'},
+    {series: 'EU07', number: '512', type: '303E', lamps: 'ZEZ', paintjob: 'PCC Intermodal', panto: 'Nożycowe', railway: 'PCC Intermodal'},
+
+    //HSL POLSKA
+    {series: 'EU07', number: '403', type: '303E', lamps: 'ZEZ', paintjob: 'Brązowe HSL', panto: 'Nożycowe', railway: 'HSL Polska'},
+    {series: 'EU07', number: '522', type: '303E', lamps: 'ZEZ', paintjob: 'Brązowe HSL', panto: 'Nożycowe', railway: 'HSL Polska'},
+
+    //ECCO Rail
+    {series: 'EU07', number: '047', type: '4E', lamps: 'ZEZ', paintjob: 'Niebieskie ECCO Rail', panto: 'Nożycowe', railway: 'ECCO Rail'},
+    {series: 'EU07', number: '088', type: '4E', lamps: 'ZEZ', paintjob: 'Niebieskie ECCO Rail', panto: 'Nożycowe', railway: 'ECCO Rail'},
+    {series: 'EU07', number: '080', type: '4E', lamps: 'ZEZ', paintjob: 'Biała Dama', panto: 'Nożycowe', railway: 'ECCO Rail'},
+    {series: 'EU07', number: '141', type: '4E', lamps: 'ZEZ', paintjob: 'Biała Dama', panto: 'Nożycowe', railway: 'ECCO Rail'},
+    {series: 'EU07', number: '145', type: '4E', lamps: 'ZEZ', paintjob: 'Biała Dama', panto: 'Nożycowe', railway: 'ECCO Rail'},
+    {series: 'EU07', number: '166', type: '4E', lamps: 'ZEZ', paintjob: 'Biała Dama', panto: 'Nożycowe', railway: 'ECCO Rail'},
+    {series: 'EU07', number: '167', type: '4E', lamps: 'ZEZ', paintjob: 'Biała Dama', panto: 'Nożycowe', railway: 'ECCO Rail'},
+    {series: 'EU07', number: '465', type: '303E', lamps: 'ZEZ', paintjob: 'Zielone Trapez', panto: 'Nożycowe', railway: 'ECCO Rail'}
+]
---
+++ baza-siodemek/scripts/main.js
@@ -0,0 +1,78 @@
+const dataShuffle = (() => {
+    const table = document.getElementById('table_content');
+
+    const loadFunction = (array) => {
+        table.innerHTML = "";
+
+        const rows = array.length;
+        const cols = 7;
+
+        for(i = 0; i < rows; i++){
+            let row = document.createElement('tr');
+
+            if(i % 2 !== 0) { row.classList.add('contrasting'); }
+
+            Object.values(array[i]).forEach(item => {
+                let data = document.createElement('td');
+                data.innerText = item;
+                row.appendChild(data);
+            })
+
+            table.appendChild(row);
+        }
+    }
+
+    loadFunction(data);
+
+    const searchData = () => {
+        let user_input = document.getElementById('search').value;
+        let select = document.getElementById('railway');
+        let results = [];
+
+        data.forEach(item => {
+            Object.values(item).forEach(entry => {
+                if(results.includes(item)) {return};
+                if(entry.toLowerCase().includes(user_input.toLowerCase())) { return results.push(item) };
+                if((item.series.toLowerCase() + "-" + item.number).includes(user_input.toLowerCase())) { return results.push(item) };
+            })
+        })
+        select.getElementsByTagName('option')[0].selected = "selected";
+        loadFunction(results);
+    }
+    
+    const fillSelect = (() => {
+        let select = document.getElementById('railway');
+        let railways = [];
+        for(i = 0; i < data.length; i++){
+            if(railways.includes(data[i].railway)) {}
+            else{railways.push(data[i].railway)};
+        }
+        railways.forEach(item => {
+            let option = document.createElement('option');
+            option.innerText = item;
+            option.value = item;
+            select.appendChild(option);
+        })
+    })();
+
+    const sortByRailway = () => {
+        let user_input = document.getElementById('railway').value;
+        let results = [];
+
+        data.forEach(item => {
+            Object.values(item).forEach(entry => {
+                if(results.includes(item)) {return};
+                if(item.railway === user_input) { return results.push(item) }
+            })
+        })
+
+        if(results.length === 0) { return loadFunction(data)};
+        loadFunction(results);
+    } 
+
+    return {
+        search: searchData,
+        sort: sortByRailway
+    }
+
+})();
